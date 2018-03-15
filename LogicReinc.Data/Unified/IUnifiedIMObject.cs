3146dc5aa79fc529377255d8c355f169fee0f263
Kelvin <logicincarnate@gmail.com>
2018-03-15 01:21:07 +0100

    Init

---
+++ LogicReinc.Data/Unified/IUnifiedIMObject.cs
@@ -0,0 +1,19 @@
+﻿using LogicReinc.Data.Unified.Attributes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicReinc.Data.Unified
+{
+    public class IUnifiedIMObject
+    {
+        virtual public string ObjectID { get; set; }
+        virtual internal IList DatabaseBase { get; }
+        virtual internal Dictionary<string, UIMPropertyState> PropertyStates { get; } = new Dictionary<string, UIMPropertyState>();
+        virtual internal List<KeyValuePair<UnifiedIMReference, IUnifiedIMObject>> RefTo { get; } = new List<KeyValuePair<UnifiedIMReference, IUnifiedIMObject>>();
+        virtual internal Type DataType { get; }
+    }
+}
